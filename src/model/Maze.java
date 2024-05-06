bb042a3f4c5734bb2893dd6893a24aee6daccbfa
sbevening <91366243+sbevening@users.noreply.github.com>
2024-05-05 17:01:27 -0700

    Utilities for Maze Class
    Overwrote toString() for Maze to produce representation of the maze's cells.
    Made Maze an iterable for Integer and created iterator that traverses through maze row by row.

--- src/model/Maze.java
+++ src/model/Maze.java
@@ -1,20 +1,25 @@
 package model;
 
-import java.awt.Color;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Maze {
+public class Maze implements Iterable<Integer> {
     public static final int EMPTY = 0;
     public static final int WALL = 1;
     public static final int TARGET = 2;
     public static final int START = 3;
 
-    private int[][] maze;
+    private final int[][] maze;
+    private final int mazeHeight;
+    private final int mazeWidth;
 
     public Maze(int[][] matrix) {
         this.maze = matrix;
         if (!isValid()) {
             throw new IllegalArgumentException();
         }
+        mazeHeight = maze.length;
+        mazeWidth = maze[0].length;
     }
 
     public int getCell(int x, int y) {
@@ -45,5 +50,53 @@ public class Maze {
         }
 
         return targetSquares == 1 && startSquares == 1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] column : maze) {
+            for (int i = 0; i < column.length; i++) {
+                if (i != 0) {
+                    sb.append(" ");
+                }
+                sb.append(column[i]);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new MazeIterator();
+    }
+
+    private class MazeIterator implements Iterator<Integer> {
+        private int yCursor = 0;
+        private int xCursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            return yCursor < maze.length && xCursor < maze.length;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Integer nextInteger = maze[yCursor][xCursor];
+
+            xCursor++;
+            if (xCursor >= mazeWidth) {
+                xCursor = 0;
+                yCursor++;
+            }
+
+            return nextInteger;
+        }
     }
 }
